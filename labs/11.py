fe8006b17db173e21928343c21fe55c826b033b5
poslam <kirden04@gmail.com>
2024-11-27 11:53:52 +1000

    11

--- labs/11.py
+++ labs/11.py
@@ -27,7 +27,6 @@ def richardson(
     """
     n = A.shape[0]
     x = np.random.rand(n)
-    x = x / np.linalg.norm(x)
 
     c = (b + a) / 2
     e = (b - a) / 2
@@ -44,29 +43,28 @@ def richardson(
 
         for j in range(k):
             y = tau[j] * (A @ y)
-            y = y / np.linalg.norm(y)
 
         lambda_new = (x.T @ A @ x) / (x.T @ x)
 
         print(f"{iter}\t{lambda_new}")
 
         if abs(lambda_new - lambda_old) < eps:
-            return lambda_new, y, iter * k + 1
+            return lambda_new, iter * k + 1
 
         x = y
         lambda_old = lambda_new
 
-    return lambda_new, y, iter * k + 1
+    return lambda_new, iter * k + 1
 
 
 size = (6, 6)
-M = generate_symmetric_pos_def_matrix(*size).astype(np.double)
+M = generate_symmetric_matrix(*size).astype(np.double)
 M /= np.max(M)
 
 print("matrix:")
 print_matrix(M)
 
-eigenval, eigenvec, iters = richardson(
+eigenval, iters = richardson(
     M,
     k=5,
     a=-1000,
@@ -75,5 +73,7 @@ eigenval, eigenvec, iters = richardson(
     max_iter=10**5,
 )
 
+mx_np_eigval = np.max(np.linalg.eigvals(M))
+
 print(f"\niters: {iters}")
-print(f"delta: {np.abs(np.max(np.linalg.eigvals(M) - eigenval))}")
+print(f"eignval delta: {np.abs(mx_np_eigval - eigenval)}")
--- labs/12.py
+++ labs/12.py
@@ -1,2 +1,57 @@
 # тема: простая итерация
 # src: методичка
+
+import sys
+
+from labs.funcs import *
+
+sys.stdout = open("./labs/output.txt", "w")
+
+
+def simple_iter(
+    A: np.ndarray,
+    x0: np.ndarray = None,
+    eps: float = 1e-10,
+    max_iter: int = 10**5,
+) -> tuple:
+    n = A.shape[0]
+    x = x0.copy() if x0 is not None else np.ones(n)
+
+    l = 0
+    for iter in range(max_iter):
+        y = A @ x
+        l_new = y @ x
+
+        y_norm = y / np.linalg.norm(y)
+
+        print(f"{iter}\t{l_new}")
+
+        if np.abs(l - l_new) < eps:
+            break
+
+        x = y_norm
+        l = l_new
+
+    return x, l, iter
+
+
+M = np.array(
+    [
+        [2, 1, 1],
+        [1, 2.5, 1],
+        [1, 1, 3],
+    ]
+)
+
+size = (6, 6)
+M = generate_symmetric_matrix(*size).astype(np.double)
+M /= np.max(M)
+
+print("matrix:")
+print_matrix(M)
+
+eigvec, eigval, iters = simple_iter(M, eps=1e-16, max_iter=10**5)
+np_mx_eigval = np.max(np.linalg.eigvals(M))
+
+print(f"\niters: {iters}")
+print(f"eigval delta: {np.abs(np_mx_eigval - eigval)}")
